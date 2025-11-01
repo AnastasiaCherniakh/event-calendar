8a99ea6c85ad39b47f01ae4baff33f33f83fcae5
AnastasiaCherniakh <anastayshablake@gmail.com>
2025-11-01 15:09:50 +0100

    Add styling to the Add Event page

--- src/pages/AddEvent.jsx
+++ src/pages/AddEvent.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './addEvent.css'
 
 export default function AddEvent({ addEvent }){
     const navigate = useNavigate();
